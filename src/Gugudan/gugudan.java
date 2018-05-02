08f3097fdc9deec8f5a7b653806fe24a773c168a
GG <GG@DESKTOP-29B447C>
2018-05-02 21:46:55 +0900

    생각없이 만들어본 반복문

--- src/Gugudan/gugudan.java
+++ src/Gugudan/gugudan.java
@@ -4,6 +4,12 @@ public class gugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 1; i >=9; i++) {
+			for(int j = 1; j >= i; j++) {
+				sum = sum + j;
+			}
+		}
 
 	}
 
