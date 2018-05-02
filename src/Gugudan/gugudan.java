958d961054b663b538dd05c9abcd9cace7574f90
GG <GG@DESKTOP-29B447C>
2018-05-03 00:02:51 +0900

    완성본

--- src/Gugudan/gugudan.java
+++ src/Gugudan/gugudan.java
@@ -1,16 +1,23 @@
 package Gugudan;
 
+import java.util.Scanner;
+
 public class gugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 0;
-		for(int i = 1; i >=9; i++) {
-			for(int j = 1; j >= i; j++) {
-				sum = sum + j;
-			}
+		
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		System.out.println("number: " +number);
+		
+		for(int i = 1; i <=9; i++) {
+			System.out.println(number*i);
 		}
-
+		
 	}
 
 }
+
+
