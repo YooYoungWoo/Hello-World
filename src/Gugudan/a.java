35704b6d6e2bdcf841b583f2a28f7abc1ba853a7
GG <GG@DESKTOP-29B447C>
2018-05-03 23:48:00 +0900

    연습용

--- src/Gugudan/a.java
+++ src/Gugudan/a.java
@@ -2,23 +2,27 @@ package Gugudan;
 
 import java.util.Scanner;
 
-public class arraygugudan {
+public class a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[10];
 		
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
-		if(number < 1) {
-			return;
-		}
-		else {
-		for(int i = 1; i <arr.length; i++) {
-			arr[i] = number * i;
-			System.out.println("result: " +arr[i]);
+		
+		class number1 {
+			int m(int number) {
+				int sum = 0;
+				for(int i = 0; i <= number; i++) {
+					sum = number * i;
+				}
+				return sum;
 			}
-	
 		}
+		int result = m.sum;
+		System.out.println("sum " );
+		
+
 	}
+
 }
